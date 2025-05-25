d780f568312dade526aaaf611fe925fbdfda6a38
chrisjustinw <christopher.william@tiket.com>
2025-05-25 13:45:45 +0700

    feat: generate continuation token

--- internal/repository/base.go
+++ internal/repository/base.go
@@ -8,6 +8,7 @@ import (
 
 type (
 	BaseRepository interface {
+		Find(ctx context.Context, collection string, findQuery map[string]interface{}) (*mongo.Cursor, error)
 		FindOne(ctx context.Context, collection string, findQuery map[string]interface{}) *mongo.SingleResult
 		ReplaceOne(ctx context.Context, collection string, query interface{}, data interface{}) (result *mongo.UpdateResult, err error)
 	}
@@ -19,6 +20,9 @@ type (
 
 func NewBaseRepository(mongoDatabase *mongo.Database) BaseRepository {
 	return &BaseRepositoryImpl{MongoDatabase: mongoDatabase}
+}
+func (m *BaseRepositoryImpl) Find(ctx context.Context, collection string, query map[string]interface{}) (*mongo.Cursor, error) {
+	return m.MongoDatabase.Collection(collection).Find(ctx, query)
 }
 
 func (m *BaseRepositoryImpl) FindOne(ctx context.Context, collection string, query map[string]interface{}) *mongo.SingleResult {
